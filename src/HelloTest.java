8ca9cc32a081f019f7cf2757f0f1fa195d1cf018
nqwrt <nqwrt@ymail.com>
2019-11-05 23:02:04 +0900

    1

---
+++ src/HelloTest.java
@@ -0,0 +1,15 @@
+class Hello{
+	
+	public void printHello(){
+		System.out.println("Hello World");
+	}	
+}
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.printHello();
+	}
+
+}
